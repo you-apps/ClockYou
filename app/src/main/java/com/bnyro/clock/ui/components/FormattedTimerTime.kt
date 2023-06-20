fcabb56039fa8506cc59dac764ea71285e9a9c8c
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-06-20 12:12:40 +0200

    feat: Add FormattedTimerTime

---
+++ app/src/main/java/com/bnyro/clock/ui/components/FormattedTimerTime.kt
@@ -0,0 +1,59 @@
+package com.bnyro.clock.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FormattedTimerTime(
+    modifier: Modifier = Modifier,
+    seconds: Int,
+) {
+    val remainingSeconds = seconds % 100
+    val minutes = seconds / 100 % 100
+    val hours = seconds / 10000 % 100
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(15.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier,
+    ) {
+        FormattedUnitTime(unit = TimeUnit.Hours, value = hours)
+        FormattedUnitTime(unit = TimeUnit.Minutes, value = minutes)
+        FormattedUnitTime(unit = TimeUnit.Seconds, value = remainingSeconds)
+    }
+}
+
+enum class TimeUnit {
+    Hours,
+    Minutes,
+    Seconds,
+}
+
+@Composable
+fun FormattedUnitTime(
+    unit: TimeUnit,
+    value: Int,
+) {
+    val isActive = value > 0
+
+    Row(
+        verticalAlignment = Alignment.Bottom,
+    ) {
+        Text(
+            text = value.toString().padStart(2, '0'),
+            color = if (isActive) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface,
+            fontSize = MaterialTheme.typography.headlineLarge.fontSize
+        )
+        Text(
+            text = unit.name.first().lowercase(),
+            color = if (isActive) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface,
+            fontSize = MaterialTheme.typography.headlineSmall.fontSize
+        )
+    }
+}
