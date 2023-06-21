a23b4830143ae2793d4bd1d645cafe7851bcf22f
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-06-21 19:46:36 +0200

    feat: Add time picker setting

--- app/src/main/java/com/bnyro/clock/util/Preferences.kt
+++ app/src/main/java/com/bnyro/clock/util/Preferences.kt
@@ -9,6 +9,7 @@ object Preferences {
 
     const val showSecondsKey = "showSeconds"
     const val themeKey = "theme"
+    const val timer_useTimePicker = "timer_useTimePicker"
 
     fun init(context: Context) {
         instance = context.getSharedPreferences("clock_you", Context.MODE_PRIVATE)
