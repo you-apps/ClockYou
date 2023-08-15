4d819c48065098db35d8f125b03011e9dac46883
Bnyro <bnyro@tutanota.com>
2023-08-15 11:44:41 +0200

    chore: bump version to v6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bnyro.clock"
         minSdk = 21
         targetSdk = 33
-        versionCode = 8
-        versionName = "5.1"
+        versionCode = 9
+        versionName = "6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
