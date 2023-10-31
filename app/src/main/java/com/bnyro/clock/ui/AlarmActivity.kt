4b52162b883b066b18d774c3e4f5f91c7fcd69c8
Suhas Dissanayake <suhasdissa@gmail.com>
2023-10-31 20:58:40 +0530

    feat: better alarm screen design

--- app/src/main/java/com/bnyro/clock/ui/AlarmActivity.kt
+++ app/src/main/java/com/bnyro/clock/ui/AlarmActivity.kt
@@ -6,23 +6,13 @@ import android.view.Window
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import com.bnyro.clock.db.DatabaseHolder
 import com.bnyro.clock.obj.Alarm
 import com.bnyro.clock.services.AlarmService
-import com.bnyro.clock.ui.theme.ClockYouTheme
+import com.bnyro.clock.ui.screens.AlarmAlertScreen
 import com.bnyro.clock.util.AlarmHelper
 import kotlinx.coroutines.runBlocking
 
@@ -32,44 +22,20 @@ class AlarmActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
 
-        val win = window
-        win.addFlags(
-            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-                or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-                or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-        )
+        window.addFlags(windowFlags)
 
         setContent {
-            ClockYouTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.SpaceEvenly,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text("Alarm", style = MaterialTheme.typography.displayLarge)
-                        alarm.label?.let {
-                            Text(text = it, style = MaterialTheme.typography.headlineMedium)
-                        }
-                        Row() {
-                            Button(onClick = {
-                                stopService(
-                                    Intent(
-                                        this@AlarmActivity.applicationContext,
-                                        AlarmService::class.java
-                                    )
-                                )
-                                this@AlarmActivity.finish()
-                            }) {
-                                Text(text = "Dismiss")
-                            }
-                        }
-                    }
-                }
-            }
+            AlarmAlertScreen(onDismiss = {
+                stopService(
+                    Intent(
+                        this@AlarmActivity.applicationContext,
+                        AlarmService::class.java
+                    )
+                )
+                this@AlarmActivity.finish()
+            }, onSnooze = {
+                /*TODO*/
+            }, label = alarm.label)
         }
 
         handleIntent(intent)
@@ -85,5 +51,10 @@ class AlarmActivity : ComponentActivity() {
         this.alarm = runBlocking {
             DatabaseHolder.instance.alarmsDao().findById(id)
         }
+    }
+
+    companion object {
+        private const val windowFlags =
+            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
     }
 }
