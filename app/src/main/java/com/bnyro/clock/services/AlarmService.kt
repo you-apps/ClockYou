146677efaaa37434fd55649dcaffd3346f4dce71
Suhas Dissanayake <suhasdissa@gmail.com>
2023-11-17 13:08:28 +0530

    feat: add notification buttons to snooze and dismiss alarm

--- app/src/main/java/com/bnyro/clock/services/AlarmService.kt
+++ app/src/main/java/com/bnyro/clock/services/AlarmService.kt
@@ -3,17 +3,19 @@ package com.bnyro.clock.services
 import android.app.Notification
 import android.app.PendingIntent
 import android.app.Service
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.media.MediaPlayer
 import android.media.RingtoneManager
 import android.net.Uri
-import android.os.Handler
 import android.os.IBinder
-import android.os.Looper
 import android.os.Vibrator
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationCompat.FOREGROUND_SERVICE_IMMEDIATE
+import androidx.core.app.ServiceCompat
 import androidx.core.net.toUri
 import com.bnyro.clock.R
 import com.bnyro.clock.db.DatabaseHolder
@@ -24,21 +26,34 @@ import com.bnyro.clock.util.NotificationHelper
 import kotlinx.coroutines.runBlocking
 
 class AlarmService : Service() {
+    private val notificationId = 5
     private var isPlaying = false
     private var vibrator: Vibrator? = null
     private var mediaPlayer: MediaPlayer? = null
     private var currentAlarm: Alarm? = null
 
-    private val handler: Handler = Handler(Looper.getMainLooper())
+    private val recorderReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            when (intent?.getStringExtra(ACTION_EXTRA_KEY)) {
+                DISMISS_ACTION -> onDestroy()
+                SNOOZE_ACTION -> {
+                    AlarmHelper.snooze(this@AlarmService, currentAlarm!!)
+                    onDestroy()
+                }
+            }
+        }
+    }
 
     override fun onCreate() {
         vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        registerReceiver(recorderReceiver, IntentFilter(ALARM_INTENT_ACTION))
         super.onCreate()
     }
 
     override fun onDestroy() {
         stop()
         Log.d("Alarm Service", "Destroying service")
+        ServiceCompat.stopForeground(this, ServiceCompat.STOP_FOREGROUND_REMOVE)
         super.onDestroy()
     }
 
@@ -52,7 +67,7 @@ class AlarmService : Service() {
         val alarm = runBlocking {
             DatabaseHolder.instance.alarmsDao().findById(id)
         }
-        startForeground(1, createNotification(this, alarm))
+        startForeground(notificationId, createNotification(this, alarm))
         play(alarm)
         currentAlarm = alarm
         return START_STICKY
@@ -127,6 +142,20 @@ class AlarmService : Service() {
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
+        val dismissIntent = Intent(ALARM_INTENT_ACTION).putExtra(ACTION_EXTRA_KEY, DISMISS_ACTION)
+        val dismissAction = NotificationCompat.Action.Builder(
+            null,
+            getString(R.string.dismiss),
+            getPendingIntent(dismissIntent, 1)
+        )
+
+        val snoozeIntent = Intent(ALARM_INTENT_ACTION).putExtra(ACTION_EXTRA_KEY, SNOOZE_ACTION)
+        val snoozeAction = NotificationCompat.Action.Builder(
+            null,
+            getString(R.string.snooze),
+            getPendingIntent(snoozeIntent, 2)
+        )
+
         val builder = NotificationCompat.Builder(context, NotificationHelper.ALARM_CHANNEL)
             .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle(alarm.label ?: context.getString(R.string.alarm))
@@ -135,6 +164,25 @@ class AlarmService : Service() {
             .setPriority(NotificationCompat.PRIORITY_MAX)
             .setCategory(NotificationCompat.CATEGORY_ALARM)
             .setFullScreenIntent(pendingIntent, true)
+            .addAction(snoozeAction.build())
+            .addAction(dismissAction.build())
+            .setForegroundServiceBehavior(FOREGROUND_SERVICE_IMMEDIATE)
+            .setOngoing(true)
         return builder.build()
+    }
+
+    private fun getPendingIntent(intent: Intent, requestCode: Int): PendingIntent =
+        PendingIntent.getBroadcast(
+            this,
+            requestCode,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+        )
+
+    companion object {
+        const val ALARM_INTENT_ACTION = "com.bnyro.clock.ALARM_ACTION"
+        const val ACTION_EXTRA_KEY = "action"
+        const val DISMISS_ACTION = "DISMISS"
+        const val SNOOZE_ACTION = "SNOOZE"
     }
 }
