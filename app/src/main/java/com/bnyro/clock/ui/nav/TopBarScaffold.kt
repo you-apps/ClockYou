85c06d0f210f00afdca61bcd9e0e17e3f7670989
fesowowako <118536485+fesowowako@users.noreply.github.com>
2024-01-09 19:20:04 +0200

    Update TopBarScaffold.kt

--- app/src/main/java/com/bnyro/clock/ui/nav/TopBarScaffold.kt
+++ app/src/main/java/com/bnyro/clock/ui/nav/TopBarScaffold.kt
@@ -9,30 +9,33 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import com.bnyro.clock.ui.components.ClickableIcon
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBarScaffold(
-    title: String,
-    onClickSettings: () -> Unit,
-    actions: @Composable (RowScope.() -> Unit) = {},
-    fab: @Composable () -> Unit = {},
-    content: @Composable (PaddingValues) -> Unit
+   title: String,
+   onClickSettings: () -> Unit,
+   actions: @Composable (RowScope.() -> Unit) = {},
+   fab: @Composable () -> Unit = {},
+   content: @Composable (PaddingValues) -> Unit
 ) {
-    Scaffold(topBar = {
-        TopAppBar(
-            title = {
-                Text(title)
-            },
-            actions = {
-                actions()
-                ClickableIcon(imageVector = Icons.Default.Settings) {
-                    onClickSettings()
-                }
-            }
-        )
-    }, floatingActionButton = fab) {
-        content(it)
-    }
+   val topAppBar = remember {
+       TopAppBar(
+           title = {
+               Text(title)
+           },
+           actions = {
+               actions()
+               ClickableIcon(imageVector = Icons.Default.Settings) {
+                  onClickSettings()
+               }
+           }
+       )
+   }
+
+   Scaffold(topBar = { topAppBar }, floatingActionButton = fab) {
+       content(it)
+   }
 }
