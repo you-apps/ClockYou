ef7e9a4814f9faf0bf7405bfc444e5f2f8944b42
fesowowako <118536485+fesowowako@users.noreply.github.com>
2024-01-09 19:02:21 +0200

    Update PermissionHelper.kt

--- app/src/main/java/com/bnyro/clock/util/PermissionHelper.kt
+++ app/src/main/java/com/bnyro/clock/util/PermissionHelper.kt
@@ -6,20 +6,14 @@ import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 
 object PermissionHelper {
-    fun checkPermissions(context: Context, permissions: Array<String>): Boolean {
-        permissions.forEach {
-            if (!hasPermission(context, it)) {
-                ActivityCompat.requestPermissions(context as Activity, arrayOf(it), 1)
-                return false
-            }
-        }
-        return true
-    }
+   fun checkPermissions(context: Context, permissions: Array<String>): Boolean {
+       return permissions.all { hasPermission(context, it) }
+   }
 
-    private fun hasPermission(context: Context, permission: String): Boolean {
-        return ActivityCompat.checkSelfPermission(
-            context,
-            permission
-        ) == PackageManager.PERMISSION_GRANTED
-    }
+   private fun hasPermission(context: Context, permission: String): Boolean {
+       return ActivityCompat.checkSelfPermission(
+           context,
+           permission
+       ) == PackageManager.PERMISSION_GRANTED
+   }
 }
