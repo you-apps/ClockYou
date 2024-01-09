7fee802a4a30700c62b335b8b06929ac51411d15
fesowowako <118536485+fesowowako@users.noreply.github.com>
2024-01-09 18:53:27 +0200

    Update AlarmReceiver.kt

--- app/src/main/java/com/bnyro/clock/receivers/AlarmReceiver.kt
+++ app/src/main/java/com/bnyro/clock/receivers/AlarmReceiver.kt
@@ -9,32 +9,45 @@ import com.bnyro.clock.db.DatabaseHolder
 import com.bnyro.clock.services.AlarmService
 import com.bnyro.clock.util.AlarmHelper
 import com.bnyro.clock.util.TimeHelper
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class AlarmReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context, intent: Intent) {
-        Log.e("receiver", "received")
-        val id = intent.getLongExtra(AlarmHelper.EXTRA_ID, -1).takeIf { it != -1L } ?: return
-        val alarm = runBlocking {
-            DatabaseHolder.instance.alarmsDao().findById(id)
-        }
+   private val dbHolder by lazy { DatabaseHolder.instance }
 
-        val currentDay = TimeHelper.getCurrentWeekDay()
+   override fun onReceive(context: Context, intent: Intent) {
+       Log.e("receiver", "received")
+       val id = intent.getLongExtra(AlarmHelper.EXTRA_ID, -1).takeIf { it != -1L } ?: return
+       GlobalScope.launch {
+           val alarm = dbHolder.alarmsDao().findById(id)
 
-        if (currentDay - 1 in alarm.days || !alarm.repeat) {
-            val playAlarm = Intent(context, AlarmService::class.java)
-            playAlarm.putExtra(AlarmHelper.EXTRA_ID, id)
-            ContextCompat.startForegroundService(context, playAlarm)
-        }
+           val currentDay = TimeHelper.getCurrentWeekDay()
 
-        // re-enqueue the alarm for the next day
-        if (alarm.repeat) {
-            AlarmHelper.enqueue(context, alarm)
-        } else {
-            alarm.enabled = false
-            runBlocking {
-                DatabaseHolder.instance.alarmsDao().update(alarm)
-            }
-        }
-    }
+           if (currentDay - 1 in alarm.days || !alarm.repeat) {
+               val playAlarm = Intent(context, AlarmService::class.java)
+               playAlarm.putExtra(AlarmHelper.EXTRA_ID, id)
+               if (!isMyServiceRunning(AlarmService::class.java, context)) {
+                  ContextCompat.startForegroundService(context, playAlarm)
+               }
+           }
+
+           // re-enqueue the alarm for the next day
+           if (alarm.repeat) {
+               AlarmHelper.enqueue(context, alarm)
+           } else {
+               alarm.enabled = false
+               dbHolder.alarmsDao().update(alarm)
+           }
+       }
+   }
+
+   private fun isMyServiceRunning(serviceClass: Class<*>, context: Context): Boolean {
+       val manager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+       for (service in manager.getRunningServices(Integer.MAX_VALUE)) {
+           if (serviceClass.name == service.service.className) {
+               return true
+           }
+       }
+       return false
+   }
 }
