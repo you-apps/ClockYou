01b479f6d5a763f3f62ed419300bdb93c029eb75
fesowowako <118536485+fesowowako@users.noreply.github.com>
2024-01-09 19:16:34 +0200

    Update StopwatchService.kt

--- app/src/main/java/com/bnyro/clock/services/StopwatchService.kt
+++ app/src/main/java/com/bnyro/clock/services/StopwatchService.kt
@@ -8,38 +8,38 @@ import com.bnyro.clock.obj.WatchState
 import com.bnyro.clock.util.NotificationHelper
 
 class StopwatchService : ScheduleService() {
-    override val notificationId = 1
+   override val notificationId = 1
 
-    override fun onCreate() {
-        super.onCreate()
-        scheduledObjects.add(
-            ScheduledObject(
-                state = mutableStateOf(WatchState.RUNNING),
-                id = notificationId
-            )
-        )
-    }
+   override fun onCreate() {
+       super.onCreate()
+       scheduledObjects.add(
+           ScheduledObject(
+               state = mutableStateOf(WatchState.RUNNING),
+               id = notificationId
+           )
+       )
+   }
 
-    override fun updateState() {
-        scheduledObjects.forEach {
-            if (it.state.value == WatchState.RUNNING) {
-                it.currentPosition.value += updateDelay
-                invokeChangeListener()
-            }
-        }
-    }
+   override fun updateState() {
+       scheduledObjects.forEach {
+           if (it.state.value == WatchState.RUNNING) {
+               it.currentPosition.value += updateDelay
+               invokeChangeListener()
+           }
+       }
+   }
 
-    override fun getNotification(scheduledObject: ScheduledObject) = NotificationCompat.Builder(
-        this,
-        NotificationHelper.STOPWATCH_CHANNEL
-    )
-        .setContentTitle(getText(R.string.stopwatch))
-        .setUsesChronometer(scheduledObject.state.value == WatchState.RUNNING)
-        .setWhen(System.currentTimeMillis() - scheduledObject.currentPosition.value)
-        .addAction(stopAction(scheduledObject))
-        .addAction(pauseResumeAction(scheduledObject))
-        .setSmallIcon(R.drawable.ic_notification)
-        .build()
+   override fun getNotification(scheduledObject: ScheduledObject) = NotificationCompat.Builder(
+       this,
+       NotificationHelper.STOPWATCH_CHANNEL
+   )
+       .setContentTitle(getText(R.string.stopwatch))
+       .setUsesChronometer(scheduledObject.state.value == WatchState.RUNNING)
+       .setWhen(System.currentTimeMillis() - scheduledObject.currentPosition.value)
+       .addAction(stopAction(scheduledObject))
+       .addAction(pauseResumeAction(scheduledObject))
+       .setSmallIcon(R.drawable.ic_notification)
+       .build()
 
-    override fun getStartNotification() = getNotification(ScheduledObject())
+   override fun getStartNotification() = getNotification(ScheduledObject())
 }
