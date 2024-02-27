3053f0e890c0ad22ab4876ecfe49224a709f4937
Suhas Dissanayake <suhasdissa@gmail.com>
2024-02-27 18:41:02 +0530

    fix: Visual glitch in alarm and clock lists

--- app/src/main/java/com/bnyro/clock/ui/screens/AlarmScreen.kt
+++ app/src/main/java/com/bnyro/clock/ui/screens/AlarmScreen.kt
@@ -112,7 +112,7 @@ fun AlarmScreen(
 
             }
 
-            items(alarms) {
+            items(items = alarms, key = { it.id }) {
                 var showDeletionDialog by remember {
                     mutableStateOf(false)
                 }
