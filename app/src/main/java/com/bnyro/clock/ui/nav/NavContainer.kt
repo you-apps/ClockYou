4ddb8f277d379b8e2804384fc1dc16d86bfa4cd9
Suhas Dissanayake <suhasdissa@gmail.com>
2024-03-12 22:09:13 +0530

    feat: improved landscape layout for stopwatch screen

--- app/src/main/java/com/bnyro/clock/ui/nav/NavContainer.kt
+++ app/src/main/java/com/bnyro/clock/ui/nav/NavContainer.kt
@@ -1,12 +1,15 @@
 package com.bnyro.clock.ui.nav
 
-import android.util.Log
+import android.content.res.Configuration
 import androidx.activity.addCallback
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.NavigationRailItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +21,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -27,7 +31,6 @@ import androidx.navigation.compose.rememberNavController
 import com.bnyro.clock.ui.MainActivity
 import com.bnyro.clock.ui.model.ClockModel
 import com.bnyro.clock.ui.model.SettingsModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 val bottomNavItems = listOf(
@@ -82,36 +85,59 @@ fun NavContainer(
         }
     }
 
+    val orientation = LocalConfiguration.current.orientation
     Scaffold(
         bottomBar = {
-            NavigationBar(
-                tonalElevation = 5.dp
-            ) {
-                bottomNavItems.forEach {
-                    NavigationBarItem(
-                        label = {
-                            Text(stringResource(it.stringRes))
-                        },
-                        icon = {
-                            Icon(it.icon, null)
-                        },
-                        selected = it == selectedRoute,
-                        onClick = {
-                            selectedRoute = it
-                            navController.navigate(it.route)
-                        }
-                    )
+            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                NavigationBar(
+                    tonalElevation = 5.dp
+                ) {
+                    bottomNavItems.forEach {
+                        NavigationBarItem(
+                            label = {
+                                Text(stringResource(it.stringRes))
+                            },
+                            icon = {
+                                Icon(it.icon, null)
+                            },
+                            selected = it == selectedRoute,
+                            onClick = {
+                                selectedRoute = it
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
                 }
             }
         }
     ) { pV ->
-        AppNavHost(
-            navController,
-            settingsModel,
-            clockModel,
-            modifier = Modifier
+        Row(
+            Modifier
                 .fillMaxSize()
                 .padding(pV)
-        )
+        ) {
+            if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                NavigationRail {
+                    bottomNavItems.forEach {
+                        NavigationRailItem(selected = it == selectedRoute,
+                            onClick = {
+                                selectedRoute = it
+                                navController.navigate(it.route)
+                            },
+                            icon = { Icon(it.icon, null) },
+                            label = {
+                                Text(stringResource(it.stringRes))
+                            })
+                    }
+                }
+            }
+            AppNavHost(
+                navController,
+                settingsModel,
+                clockModel,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
+        }
     }
 }
