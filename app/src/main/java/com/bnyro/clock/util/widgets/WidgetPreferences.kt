48853620ff5a140d7a3aa3a1e69e4aaeb13654b2
Suhas Dissanayake <suhasdissa@gmail.com>
2024-04-19 18:55:57 +0530

    feat: Analog clock configuration

--- app/src/main/java/com/bnyro/clock/util/widgets/WidgetPreferences.kt
+++ app/src/main/java/com/bnyro/clock/util/widgets/WidgetPreferences.kt
@@ -15,3 +15,10 @@ internal const val PREF_DATE_TEXT_SIZE = "dateTextSize:"
 internal const val PREF_TIME_TEXT_SIZE = "timeTextSize:"
 internal const val PREF_TIME_ZONE = "timeZone:"
 internal const val PREF_TIME_ZONE_NAME = "timeZoneName:"
+
+// Analog Clock widget
+internal const val PREF_CLOCK_HOUR_HAND = "analogClockHour:"
+internal const val PREF_CLOCK_MINUTE_HAND = "analogClockMinute:"
+internal const val PREF_CLOCK_SECOND_HAND = "analogClockSecond:"
+internal const val PREF_CLOCK_DIAL = "analogClockDial:"
+internal const val PREF_CLOCK_FACE_NAME = "analogClockFaceName:"
