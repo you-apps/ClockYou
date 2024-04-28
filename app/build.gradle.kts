7c4ed0f58766856b05267d9f8d54493e67ae9740
Suhas Dissanayake <suhasdissa@gmail.com>
2024-04-28 19:47:31 +0530

    chore: bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.bnyro.clock"
         minSdk = 21
         targetSdk = 34
-        versionCode = 15
-        versionName = "7.1"
+        versionCode = 16
+        versionName = "8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
