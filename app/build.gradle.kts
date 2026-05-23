352c76a97ec367ec12e277effcca79da4ba1e413
Elektron123 <147244249+Elektron123@users.noreply.github.com>
2026-05-23 13:42:18 +0200

    chore: update some dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
