74f8d6dc11628a1047f9cbb0650ef1a3212cdb29
Elektron123 <147244249+Elektron123@users.noreply.github.com>
2026-07-12 11:35:01 +0200

    fix: 3 button navigation messes up the plus button position

--- app/src/main/java/com/bnyro/clock/navigation/TopBarScaffold.kt
+++ app/src/main/java/com/bnyro/clock/navigation/TopBarScaffold.kt
@@ -2,6 +2,7 @@ package com.bnyro.clock.navigation
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -11,17 +12,21 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import com.bnyro.clock.presentation.components.ClickableIcon
 import androidx.compose.material3.FabPosition
+import androidx.compose.ui.Modifier
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopBarScaffold(
     title: String,
     onClickSettings: () -> Unit,
+    modifier: Modifier = Modifier,
     actions: @Composable (RowScope.() -> Unit) = {},
     fab: @Composable () -> Unit = {},
     fabPosition: FabPosition = FabPosition.End,
     content: @Composable (PaddingValues) -> Unit
 ) {
     Scaffold(
+        modifier = modifier.fillMaxSize(),
+        contentWindowInsets = androidx.compose.foundation.layout.WindowInsets(0, 0, 0, 0),
         topBar = {
             TopAppBar(title = { Text(title) }, actions = {
                 actions()
@@ -29,8 +34,10 @@ fun TopBarScaffold(
                     onClickSettings()
                 }
             })
-        }, floatingActionButton = fab, floatingActionButtonPosition = fabPosition
-    ) {
+        },
+        floatingActionButton = fab,
+        floatingActionButtonPosition = fabPosition
+    ) { it ->
         content(it)
     }
 }
