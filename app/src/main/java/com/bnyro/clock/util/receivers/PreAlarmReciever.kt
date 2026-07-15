720cb331702af6dca1f4ac6e7bef0d3ea20bff88
Elektron123 <147244249+Elektron123@users.noreply.github.com>
2026-07-15 14:04:16 +0200

    feat: improve skip alarm notification
    Issue #510

--- app/src/main/java/com/bnyro/clock/util/receivers/PreAlarmReciever.kt
+++ app/src/main/java/com/bnyro/clock/util/receivers/PreAlarmReciever.kt
@@ -7,10 +7,15 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.text.format.DateUtils
 import androidx.core.app.NotificationCompat
+import com.bnyro.clock.App
 import com.bnyro.clock.R
 import com.bnyro.clock.util.AlarmHelper
-import kotlin.jvm.java
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class PreAlarmReceiver : BroadcastReceiver() {
     companion object {
@@ -28,8 +33,6 @@ class PreAlarmReceiver : BroadcastReceiver() {
             notificationManager.createNotificationChannel(channel)
         }
 
-
-
         val dismissIntent = Intent(context, DismissUpcomingReceiver::class.java).apply {
             putExtra(AlarmHelper.EXTRA_ID, id)
         }
@@ -40,16 +43,38 @@ class PreAlarmReceiver : BroadcastReceiver() {
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
-        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+        val pendingResult = goAsync()
+        CoroutineScope(Dispatchers.Default).launch {
+            try {
+                val alarmRepository = (context.applicationContext as App).container.alarmRepository
+                val alarm = alarmRepository.getAlarmById(id)
 
-            .setSmallIcon(R.drawable.ic_alarm)
-            .setContentTitle("Upcoming Alarm")
+                if (alarm != null) {
+                    val targetAlarmTimeMs = AlarmHelper.getAlarmTime(alarm)
 
-             .setPriority(NotificationCompat.PRIORITY_LOW)
-             .addAction(R.drawable.ic_alarm, "Skip", dismissPendingIntent)
-             .setAutoCancel(true)
-             .build()
+                    val formattedTime = DateUtils.formatDateTime(
+                        context,
+                        targetAlarmTimeMs,
+                        DateUtils.FORMAT_SHOW_TIME
+                    )
 
-        notificationManager.notify(id.toInt() + PRE_ALARM_OFFSET, notification)
+                    val contentText = context.getString(R.string.upcoming_alarm_content, formattedTime)
+                    withContext(Dispatchers.Main) {
+                        val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+                            .setSmallIcon(R.drawable.ic_alarm)
+                            .setContentTitle(context.getString(R.string.upcoming_alarm))
+                            .setContentText(contentText)
+                            .setPriority(NotificationCompat.PRIORITY_LOW)
+                            .addAction(R.drawable.ic_alarm, context.getString(R.string.skip), dismissPendingIntent)
+                            .setAutoCancel(true)
+                            .build()
+
+                        notificationManager.notify(id.toInt() + PRE_ALARM_OFFSET, notification)
+                    }
+                }
+            } finally {
+                pendingResult.finish()
+            }
+        }
     }
 }
