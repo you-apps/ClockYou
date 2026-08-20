cb00e4fce8ff7ff0e3c0246d1c7d9ae0d84c3943
Elektron123 <147244249+Elektron123@users.noreply.github.com>
2026-08-20 19:19:21 +0200

    improve? new new alarm button

--- app/src/main/java/com/bnyro/clock/presentation/screens/alarm/AlarmScreen.kt
+++ app/src/main/java/com/bnyro/clock/presentation/screens/alarm/AlarmScreen.kt
@@ -104,29 +104,19 @@ fun AlarmScreen(
                             Icon(Icons.Rounded.Add, null)
                         }
                         VerticalDivider(
-                            modifier = Modifier.fillMaxHeight(),
-                            color = MaterialTheme.colorScheme.onPrimaryContainer
+                            modifier = Modifier
+                                .padding(vertical = 12.dp)
+                                .width(1.dp),
+                            color = MaterialTheme.colorScheme.onSurfaceVariant.copy(alpha = 0.2f)
                         )
                         Box(
                             modifier = Modifier
                                 .fillMaxHeight()
                                 .width(FAB_SIZE / 2)
-                                .clickable { showAlarmKinds = true },
+                                .clickable { onAlarm.invoke(0L, true) },
                             contentAlignment = Alignment.Center
                         ) {
                             Icon(Icons.Rounded.ExpandLess, null)
-                            DropdownMenu(
-                                expanded = showAlarmKinds,
-                                onDismissRequest = { showAlarmKinds = false }
-                            ) {
-                                DropdownMenuItem(
-                                    text = { Text(stringResource(R.string.add_advanced_alarm)) },
-                                    onClick = {
-                                        showAlarmKinds = false
-                                        onAlarm.invoke(0L, true)
-                                    }
-                                )
-                            }
                         }
                     }
                 }
